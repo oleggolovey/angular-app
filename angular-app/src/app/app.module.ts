94491e31fb8eeea79c52d05e13a44a1699f7c79d
Oleh Golovey <oledzho1998@gmail.com>
2021-10-27 16:26:57 +0300

    initial commit

---
+++ angular-app/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MovieService } from './services/movie.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [MovieService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-app/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  BASE_URL: "https://api.themoviedb.org/3",
+  IMG_URL: "https://image.tmdb.org/t/p/w500",
+  API_KEY: // Your API v4 key
+};
